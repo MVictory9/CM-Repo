e73e798b7f7aba3a0a005d5211fd2c53cb007d21
MVictory9 <78539513+MVictory9@users.noreply.github.com>
2021-02-23 01:56:46 +0200

    Java Task 1 Welcome Message
    Create Program that print welcome message to your customer

---
+++ UserInputs.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+	public class UserInputs {
+	
+	public static void main(String[] args) {
+		
+		Scanner takeInputs = new Scanner(System.in);
+		System.out.println("Please Enter your full name: ");
+		String UserInputs = takeInputs.next();
+		System.out.print("Wlecome Dear customer: "+UserInputs);
+		
+		
+	}
+
+}
+	
+
